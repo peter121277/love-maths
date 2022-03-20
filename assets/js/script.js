674a882ef04cd454d4ae346a12284d246f475cf0
peter121277 <piotr1977@interia.eu>
2022-03-20 02:49:54 +0000

    Add code to check the answer and respond to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,6 +52,11 @@ function checkAnswer() {
 }
 
 function calculateCorrectAnswer() {
+
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator").innerText;
+
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
